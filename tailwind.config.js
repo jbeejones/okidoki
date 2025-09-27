34d99a8e19f2a71de1da3c101f370256dcc12428
restdbjones <jones@restdb.io>
2025-09-27 10:42:48 +0200

    Add missing css for custom pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,49 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+    require('daisyui')
   ],
+  
+  // DaisyUI config for v3
+  daisyui: {
+    themes: [
+      'light',
+      'dark', 
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset'
+    ],
+    darkTheme: 'dark',
+    base: true,
+    styled: true,
+    utils: true,
+  },
 } 
